fd2b6a880498f6339d0bd738737a761f425c82ff
ypc2e55orj <ypc2e55orj@gmail.com>
2024-03-18 18:13:35 +0900

    最低速度を追加

--- App/Inc/parameter.h
+++ App/Inc/parameter.h
@@ -10,46 +10,54 @@
 #define NUM_PARAMETERS 3
 
 /* Parameter 0 ---------------------------------------------------------------*/
-#define PARAMETER_0_MAX_VELOCITY 1.0     // [m/s]
-#define PARAMETER_0_MAX_ACCELERATION 1.5 // [m/ss]
+#define PARAMETER_0_MIN_VELOCITY 0.1 // [m/s]
+#define PARAMETER_0_MAX_VELOCITY 1.0 // [m/s]
+#define PARAMETER_0_ACCELERATION 1.5 // [m/ss]
 #define PARAMETER_0_VELOCITY_PID_KP 10.0
 #define PARAMETER_0_VELOCITY_PID_KI 0.0
 #define PARAMETER_0_VELOCITY_PID_KD 0.0
-#define PARAMETER_0_MAX_ANGULAR_VELOCITY 10.0     // [rad/s]
-#define PARAMETER_0_MAX_ANGULAR_ACCELERATION 20.0 // [rad/ss]
+#define PARAMETER_0_MIN_ANGULAR_VELOCITY 10.0 // [rad/s]
+#define PARAMETER_0_MAX_ANGULAR_VELOCITY 10.0 // [rad/s]
+#define PARAMETER_0_ANGULAR_ACCELERATION 20.0 // [rad/ss]
 #define PARAMETER_0_ANGULAR_VELOCITY_PID_KP 0.6
 #define PARAMETER_0_ANGULAR_VELOCITY_PID_KI 0.05
 #define PARAMETER_0_ANGULAR_VELOCITY_PID_KD 0.0
 
 /* Parameter 1 ---------------------------------------------------------------*/
-#define PARAMETER_1_MAX_VELOCITY 0.1     // [m/s]
-#define PARAMETER_1_MAX_ACCELERATION 1.0 // [m/ss]
+#define PARAMETER_1_MIN_VELOCITY 0.1 // [m/s]
+#define PARAMETER_1_MAX_VELOCITY 0.1 // [m/s]
+#define PARAMETER_1_ACCELERATION 1.0 // [m/ss]
 #define PARAMETER_1_VELOCITY_PID_KP 1.0
 #define PARAMETER_1_VELOCITY_PID_KI 0.0
 #define PARAMETER_1_VELOCITY_PID_KD 0.0
-#define PARAMETER_1_MAX_ANGULAR_VELOCITY 1.0     // [rad/s]
-#define PARAMETER_1_MAX_ANGULAR_ACCELERATION 1.0 // [rad/ss]
+#define PARAMETER_1_MIN_ANGULAR_VELOCITY 1.0 // [rad/s]
+#define PARAMETER_1_MAX_ANGULAR_VELOCITY 1.0 // [rad/s]
+#define PARAMETER_1_ANGULAR_ACCELERATION 1.0 // [rad/ss]
 #define PARAMETER_1_ANGULAR_VELOCITY_PID_KP 1.0
 #define PARAMETER_1_ANGULAR_VELOCITY_PID_KI 0.0
 #define PARAMETER_1_ANGULAR_VELOCITY_PID_KD 0.0
 
 /* Parameter 2 ---------------------------------------------------------------*/
-#define PARAMETER_2_MAX_VELOCITY 0.3     // [m/s]
-#define PARAMETER_2_MAX_ACCELERATION 1.5 // [m/ss]
+#define PARAMETER_2_MIN_VELOCITY 0.3 // [m/s]
+#define PARAMETER_2_MAX_VELOCITY 0.3 // [m/s]
+#define PARAMETER_2_ACCELERATION 1.5 // [m/ss]
 #define PARAMETER_2_VELOCITY_PID_KP 10.0
 #define PARAMETER_2_VELOCITY_PID_KI 0.0
 #define PARAMETER_2_VELOCITY_PID_KD 0.0
-#define PARAMETER_2_MAX_ANGULAR_VELOCITY 10.0     // [rad/s]
-#define PARAMETER_2_MAX_ANGULAR_ACCELERATION 20.0 // [rad/ss]
+#define PARAMETER_2_MIN_ANGULAR_VELOCITY 10.0 // [rad/s]
+#define PARAMETER_2_MAX_ANGULAR_VELOCITY 10.0 // [rad/s]
+#define PARAMETER_2_ANGULAR_ACCELERATION 20.0 // [rad/ss]
 #define PARAMETER_2_ANGULAR_VELOCITY_PID_KP 0.6
 #define PARAMETER_2_ANGULAR_VELOCITY_PID_KI 0.0
 #define PARAMETER_2_ANGULAR_VELOCITY_PID_KD 0.0
 
 typedef struct
 {
+  float min_velocity;
   float max_velocity;
   float acceleration;
   float velocity_pid[3];
+  float min_angular_velocity;
   float max_angular_velocity;
   float angular_acceleration;
   float angular_velocity_pid[3];
--- App/Src/parameter.c
+++ App/Src/parameter.c
@@ -13,13 +13,15 @@ void PARAMETER_Init()
 #define SET_PARAMETER(num)                                                               \
   do                                                                                     \
   {                                                                                      \
+    parameters[num].min_velocity = PARAMETER_##num##_MIN_VELOCITY;                       \
     parameters[num].max_velocity = PARAMETER_##num##_MAX_VELOCITY;                       \
-    parameters[num].acceleration = PARAMETER_##num##_MAX_ACCELERATION;                   \
+    parameters[num].acceleration = PARAMETER_##num##_ACCELERATION;                       \
     parameters[num].velocity_pid[0] = PARAMETER_##num##_VELOCITY_PID_KP;                 \
     parameters[num].velocity_pid[1] = PARAMETER_##num##_VELOCITY_PID_KI;                 \
     parameters[num].velocity_pid[2] = PARAMETER_##num##_VELOCITY_PID_KD;                 \
+    parameters[num].min_angular_velocity = PARAMETER_##num##_MIN_ANGULAR_VELOCITY;       \
     parameters[num].max_angular_velocity = PARAMETER_##num##_MAX_ANGULAR_VELOCITY;       \
-    parameters[num].angular_acceleration = PARAMETER_##num##_MAX_ANGULAR_ACCELERATION;   \
+    parameters[num].angular_acceleration = PARAMETER_##num##_ANGULAR_ACCELERATION;       \
     parameters[num].angular_velocity_pid[0] = PARAMETER_##num##_ANGULAR_VELOCITY_PID_KP; \
     parameters[num].angular_velocity_pid[1] = PARAMETER_##num##_ANGULAR_VELOCITY_PID_KI; \
     parameters[num].angular_velocity_pid[2] = PARAMETER_##num##_ANGULAR_VELOCITY_PID_KD; \
